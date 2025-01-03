4b63bd1fba321c45b2f685c278f97b85f0f9af87
anurag721721 <anurag.yadav@aayaninfotech.com>
2025-01-03 17:06:12 +0530

    port change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const apiRoutes = require("./apiRoutes");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7777;
 
 app.use(cors());
 app.use(express.json());
