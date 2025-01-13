8ce8816249fefae9d632b700a9a7e75eec47a9d8
Aayaninfotech <Aayaninfotech>
2025-01-13 15:04:25 +0530

    user api magement

--- AdmRts/userRoutes.js
+++ AdmRts/userRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllUsers,
+  getUsersByType,
   getUserById,
   updateUser,
   deleteUser,
 } = require("../AdmCtrl/userController");
 
 router.get("/", getAllUsers);
+router.get("/type/:hunte/pagelimit/:limit", getUsersByType);
 router.get("/:id", getUserById);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
