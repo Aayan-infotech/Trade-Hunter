ad1fb9909563df8281a1301f506dc3a474d86dca
Aayaninfotech <Aayaninfotech>
2025-02-21 15:45:23 +0530

    logOut api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -50,3 +50,48 @@ router.get("/profiles/hunter", verifyUser, getHunterProfile);
 
 
 module.exports = router;
+
+
+/*
+const express = require("express");
+const {
+  signUp,
+  login,
+  logout,
+  verifyEmail,
+  verifyOtp,
+  forgotPassword,
+  resetPasswordWithOTP,
+  changePassword,
+  getProviderProfile,
+  getHunterProfile,
+} = require("../controllers/authController");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer();
+const { uploadToS3 } = require("../common/multerConfig");
+const { refreshToken } = require("../middlewares/auth");
+const { verifyUser } = require("../middlewares/auth");
+router.post("/signup", 
+  upload.single("images"),
+  async (req, res, next) => {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded." });
+    }
+    next();
+  },
+  uploadToS3,
+  signUp
+);
+router.post("/login", login);
+router.post("/logout", verifyUser, logout);
+router.post("/verify-email", verifyEmail);
+router.post("/verify-otp", verifyOtp);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password-with-otp", resetPasswordWithOTP);
+router.post("/changePassword/:id", changePassword);
+router.post("/refreshtoken", refreshToken);
+router.get("/getProviderProfile", verifyUser, getProviderProfile);
+router.get("/getHunterProfile", verifyUser, getHunterProfile);
+module.exports = router;
+*/
