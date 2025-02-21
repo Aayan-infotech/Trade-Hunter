15f1dd171152b7722a2a325b78ad4f26acf8c940
Aayaninfotech <Aayaninfotech>
2025-02-21 16:35:56 +0530

    logOut api

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/jobs", jobRts)
 app.use("/api/notification",notificationRts);
 app.use("/api/count",dashboardRts)
 
-
+ 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
