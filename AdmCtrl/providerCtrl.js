f82350b3f85f80b1830336a830a683a843acb467
Aayaninfotech <Aayaninfotech>
2025-02-24 11:38:50 +0530

    s3

--- AdmCtrl/providerCtrl.js
+++ AdmCtrl/providerCtrl.js
@@ -2,9 +2,10 @@ const Provider = require("../models/providerModel");
 
 exports.getAllProviders = async (req, res) => {
   try {
-    const { limit = 10, page = 1, search = "", userStatus } = req.query;
+    const { limit = 10, page = 1, search = "", userStatus, isGuestMode = false } = req.query;
 
     let query = {
+      isGuestMode: false,
       $or: [
         { contactName: { $regex: search, $options: "i" } },
         { email: { $regex: search, $options: "i" } },
@@ -39,7 +40,6 @@ exports.getAllProviders = async (req, res) => {
     res.status(500).json({ success: false, message: "Server error", error });
   }
 };
-
 
 // Delete a Provider
 exports.deleteProvider = async (req, res) => {
@@ -76,6 +76,48 @@ exports.updateProvider = async (req, res) => {
       success: true,
       message: "Provider updated successfully",
       data: updatedProvider,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error", error });
+  }
+};
+
+
+exports.getAllProvidersGuestMode = async (req, res) => {
+  try {
+    const { limit = 10, page = 1, search = "", userStatus, isGuestMode = false } = req.query;
+
+    let query = {
+      isGuestMode: true,
+      $or: [
+        { contactName: { $regex: search, $options: "i" } },
+        { email: { $regex: search, $options: "i" } },
+      ],
+    };
+
+    const validStatuses = ["Active", "Suspended", "Pending"];
+    if (userStatus && validStatuses.includes(userStatus)) {
+      query.userStatus = userStatus;
+    }
+
+    const providers = await Provider.find(query)
+      .skip((page - 1) * parseInt(limit))
+      .limit(parseInt(limit))
+      .select(
+        "businessName contactName email phoneNo userType userStatus emailVerified documentStatus subscriptionStatus"
+      );
+
+    const totalProviders = await Provider.countDocuments(query);
+
+    res.status(200).json({
+      success: true,
+      data: providers,
+      metadata: {
+        total: totalProviders,
+        page: parseInt(page),
+        limit: parseInt(limit),
+        totalPages: Math.ceil(totalProviders / limit),
+      },
     });
   } catch (error) {
     res.status(500).json({ success: false, message: "Server error", error });
--- AdmRts/providerRts.js
+++ AdmRts/providerRts.js
@@ -5,5 +5,6 @@ const providerController = require("../AdmCtrl/providerCtrl");
 router.get("/", providerController.getAllProviders);
 router.delete("/:id", providerController.deleteProvider);
 router.put("/:id", providerController.updateProvider);
+router.get("/GuestMode", providerController.getAllProvidersGuestMode);
 
 module.exports = router;
