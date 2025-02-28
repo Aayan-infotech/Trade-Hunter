8828790d9c8702a18e4d2024f5e4eb2b5f0c2dc1
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-02-28 10:51:20 +0530

    Upload api

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const fileController = require('../controllers/providerController');
+const { uploadToS3 } = require("../common/multerConfig");
 
 // Route to handle file upload
-router.post('/upload/:providerId', fileController.uploadFile);
+router.post('/upload/:providerId',uploadToS3, fileController.uploadFile);
 router.post('/getProviderLocation',fileController.getProviderByUserLocation);
 router.post('/getJobsForGuest',fileController.getJobsForGuest); 
 router.post('/getJobs',fileController.getJobs); 
