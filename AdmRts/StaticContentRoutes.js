1a98a3400b4e18db38029011d2bc6c00478ae2fd
Aayaninfotech <Aayaninfotech>
2025-03-03 12:26:25 +0530

    add  edite

--- AdmRts/StaticContentRoutes.js
+++ AdmRts/StaticContentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const contentController = require('../controllers/StaticContentControllers');
+const contentController = require('../AdmCtrl/StaticContentControllers');
 
 
 router.get('/:section', contentController.getContent);
