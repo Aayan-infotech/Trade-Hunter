5cfb997ea8b6028c377b9e4fb8d090b55b05b44a
Aayaninfotech <Aayaninfotech>
2025-03-03 14:00:12 +0530

    add  edite

--- controllers/authController.js
+++ controllers/authController.js
@@ -108,7 +108,7 @@ const signUp = async (req, res) => {
       }
       return res.status(400).json({ message: "User already exists." });
     }
-
+    
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
