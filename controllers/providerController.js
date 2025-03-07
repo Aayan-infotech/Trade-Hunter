1413de88ee83b7d9bb807d9e83f88fb49b00a4da
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-03-07 17:18:45 +0530

    Added delete document api

--- controllers/providerController.js
+++ controllers/providerController.js
@@ -748,9 +748,9 @@ exports.deleteFile = async (req, res) => {
     }
     
     await foundProvider.save();
-    return res.status(200).json({ message: "File deleted successfully." });
+    return res.status(200).json({ status:200,message: "File deleted successfully." });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error: error.message });
+    return res.status(500).json({ status:500, message: "Internal server error", error: error.message });
   }
 };
 
