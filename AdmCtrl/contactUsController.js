1264ba5c765e06036a2fd5fca578dfb8c1da2267
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-03-17 11:18:21 +0530

    Delete follow up api

--- AdmCtrl/contactUsController.js
+++ AdmCtrl/contactUsController.js
@@ -26,3 +26,16 @@ exports.createContact = async (req, res) => {
       res.status(500).json({ message: "Internal server error", error: error.message });
     }
   };
+
+  exports.deleteContact = async (req, res) => {
+    try {
+      const contactId = req.params.id;
+      const deletedContact = await Contact.findByIdAndDelete(contactId);
+      if (!deletedContact) {
+        return res.status(404).json({ message: "Contact not found" });
+      }
+      res.status(200).json({ message: "Contact deleted successfully", contact: deletedContact });
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+  };
--- AdmRts/contactUsRts.js
+++ AdmRts/contactUsRts.js
@@ -4,6 +4,7 @@ const ContactUsController = require('../AdmCtrl/contactUsController');
 
 router.post("/send", ContactUsController.createContact);
 router.get("/getAll", ContactUsController.getAllContacts);
+router.delete("/delete/:id",ContactUsController.deleteContact);
 
 
 module.exports = router;
