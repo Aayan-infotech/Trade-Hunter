6648115a91f04b39a154b61c7560b4df1e88c806
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-03-20 13:35:12 +0530

    changes

--- common/multerConfig.js
+++ common/multerConfig.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { S3, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3 } = require("@aws-sdk/client-s3");
 const {
   SecretsManagerClient,
   GetSecretValueCommand,
@@ -32,7 +32,10 @@ const getS3Client = async () => {
   try {
     const credentials = await getAwsCredentials();
     return new S3({
-      credentials, // use the credentials from Secrets Manager
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey:  process.env.AWS_SECRET_ACCESS_KEY,
+      },
       region: process.env.AWS_REGION,
     });
   } catch (error) {
@@ -41,19 +44,18 @@ const getS3Client = async () => {
 };
 
 const uploadToS3 = async (req, res, next) => {
-  if (!req.file) {
-    console.log("No file uploaded, skipping S3 upload.");
-    return next();
-  }
-
   const s3 = await getS3Client();
+
   try {
+    console.log(req.file)
     const file = req.file;
+
     const files = Array.isArray(file) ? file : [file];
     const fileLocations = [];
+    console.log(file)
 
     for (const file of files) {
-      console.log("Uploading file:", file.originalname);
+
       const params = {
         Bucket: process.env.AWS_S3_BUCKET_NAME,
         Key: `${Date.now()}-${file.originalname}`,
@@ -61,17 +63,14 @@ const uploadToS3 = async (req, res, next) => {
         ContentType: file.mimetype,
       };
 
-      // Use AWS SDK v3's command-based approach
-      await s3.send(new PutObjectCommand(params));
+      await s3.putObject(params);
       const fileUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`;
-      console.log("Uploaded file URL:", fileUrl);
       fileLocations.push(fileUrl);
     }
 
     req.fileLocations = fileLocations;
     next();
   } catch (uploadError) {
-    console.error("S3 upload error:", uploadError);
     return res.status(500).send(uploadError.message);
   }
 };
