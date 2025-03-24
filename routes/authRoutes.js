b29ab2f6a3d18a998d75f4749010a7ab3bcdae73
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-03-24 11:47:10 +0530

    changes in api for jobPost document

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -90,7 +90,13 @@ router.post("/changePassword/:id", changePassword);
 router.post("/refreshtoken", refreshToken);
 router.get("/getProviderProfile", verifyUser, getProviderProfile);
 router.get("/getHunterProfile", verifyUser, getHunterProfile);
-router.put("/update/:id",upload.single("images"),uploadToS3, verifyUser, updateUserById)
+router.put(
+  "/update/:id",
+  upload.any(),         
+  uploadToS3, 
+  verifyUser, 
+  updateUserById
+);
 router.get("/recentSignups", getNewSignups)
 
 
