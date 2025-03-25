3bfbb84d787b0f598706eacae6689ec224c86ba8
Aayaninfotech <Aayaninfotech>
2025-03-25 12:56:51 +0530

    api chnges

--- apiRoutes.js
+++ apiRoutes.js
@@ -4,8 +4,6 @@ const serviceRoutes = require("./routes/serviceRoutes");
 const providerRoutes = require("./routes/providerRoutes");
 const jobpostRoutes = require("./routes/jobpostRoutes");
 const addressRoutes = require("./routes/addressRoute");
-const subscriptionRoutes = require("./routes/subscriptionRoutes");
-const paymentRoutes = require("./routes/paymentRoutes");
 
 const router = express.Router();
 
@@ -14,7 +12,5 @@ router.use("/service", serviceRoutes);
 router.use("/provider", providerRoutes);
 router.use("/jobpost",jobpostRoutes);
 router.use("/address", addressRoutes);
-router.use("/subscription", subscriptionRoutes);
-router.use("/payment", paymentRoutes);
 
 module.exports = router;
