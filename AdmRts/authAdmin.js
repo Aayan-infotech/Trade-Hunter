8aeb22070dbbf9f770fda0d2cc989f7a32f59704
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-03-26 15:49:32 +0530

    liitle change in get recent jobPosts api

--- AdmRts/authAdmin.js
+++ AdmRts/authAdmin.js
@@ -2,23 +2,22 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 
-
 const HARD_EMAIL = "aayaninfotech@gmail.com";
 const HARD_PASSWORD = "aayaninfotech";
-const AdminId = "YWF5YW5pbmZvdGVjaEBnbWFpbC5jb20="
+const AdminId = "YWF5YW5pbmZvdGVjaEBnbWFpbC5jb20=";
 
 // POST /api/auth/login
 router.post("/login", (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (email !== HARD_EMAIL || password !== HARD_PASSWORD) {
-        return res.status(401).json({ message: "Invalid credentials" });
-    }
+  if (email !== HARD_EMAIL || password !== HARD_PASSWORD) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
 
-    const payload = { email };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1m" });
+  const payload = { email };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1m" });
 
-    res.json({ token });
+  res.json({ token, adminId: AdminId });
 });
 
 module.exports = router;
