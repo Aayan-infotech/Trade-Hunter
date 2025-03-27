73ebad81dcf2f2957d3bd4ccd6d6ac1e0b6eae3b
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-03-27 10:24:01 +0530

    Added changes

--- AdmRts/providerRts.js
+++ AdmRts/providerRts.js
@@ -8,8 +8,8 @@ const { uploadToS3 } = require("../common/multerConfig");
 const providers = require('../controllers/providerController');
 
 router.get("/", providerController.getAllProviders);
-router.delete("/:id", providerController.deleteProvider);
-router.put("/delete/:id", providerController.updateProvider);
+router.delete("/delete/:id", providerController.deleteProvider);
+router.put("/:id", providerController.updateProvider);
 router.get("/GuestMode", providerController.getAllProvidersGuestMode);
 
 router.put('/updateById/:id',upload.single("images"), uploadToS3, providers.updateProviderById);
