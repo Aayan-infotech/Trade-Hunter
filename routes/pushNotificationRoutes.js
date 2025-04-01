3600006524f37cc5beba30530654fbe32b1b7ade
Aayaninfotech <Aayaninfotech>
2025-04-01 18:03:50 +0530

    make api device token

--- routes/pushNotificationRoutes.js
+++ routes/pushNotificationRoutes.js
@@ -5,7 +5,7 @@ const { verifyUser } = require("../middlewares/auth");
 
 router.post('/send-notification',verifyUser, notificationController.sendPushNotification);
 router.get('/get-notification',verifyUser, notificationController.getNotificationsByUserId);
-router.get('/Read-notification',verifyUser, notificationController.ReadNotification);
+router.get('/Read-notification/:notificationId',verifyUser, notificationController.ReadNotification);
 router.get('/AllRead-notification',verifyUser, notificationController.AllReadNotifications);
 
 
