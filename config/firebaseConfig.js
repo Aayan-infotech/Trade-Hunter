be55be4ad8e0cddd47bf42a7f0ddcfe3bf88f039
Aayaninfotech <Aayaninfotech>
2025-04-02 11:51:56 +0530

    make api device token

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,6 +1,15 @@
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./trade-hunter-873e9-firebase-adminsdk-fbsvc-ef94b3484d.json"); // Downloaded from Firebase Console
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+// module.exports = admin;
+
+
 const admin = require("firebase-admin");
-const serviceAccount = require("./trade-hunter-873e9-firebase-adminsdk-fbsvc-ef94b3484d.json"); // Downloaded from Firebase Console
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(process.env.SECRET_NAME),
 });
+
 module.exports = admin;
