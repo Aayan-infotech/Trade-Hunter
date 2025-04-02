c1a919b9a77f0547f2fd74ea3d00d520e8ff6cfa
Aayaninfotech <Aayaninfotech>
2025-04-02 12:43:51 +0530

    make api device token

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -6,10 +6,47 @@
 // module.exports = admin;
 
 
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
+
 const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert(process.env.SECRET_NAME),
+const secretsManagerClient = new SecretsManagerClient({
+  region: 'us-east-1',
+});
+
+const getFirebaseCredentials = async () => {
+  try {
+    const command = new GetSecretValueCommand({ SecretId: "trade-hunter" });
+    const data = await secretsManagerClient.send(command);
+
+    if (!data.SecretString) {
+      throw new Error("No secret found in AWS Secrets Manager");
+    }
+    return JSON.parse(data.SecretString);
+  } catch (error) {
+    console.error("Failed to retrieve Firebase credentials", error);
+    return null;
+  }
+};
+
+const initializeFirebase = async () => {
+  const serviceAccount = await getFirebaseCredentials();
+  if (!serviceAccount) {
+    throw new Error("Firebase service account not found in AWS Secrets Manager");
+  }
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+  console.log("Firebase Initialized Successfully");
+};
+
+initializeFirebase().catch((err) => {
+  console.error("Firebase initialization failed", err);
 });
 
 module.exports = admin;
