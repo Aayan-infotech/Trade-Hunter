32b8c8213d598eb68e6503d2fac2f3964c9d1981
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-04-04 16:30:04 +0530

    Added req.body in getProvidersByBusinessType

--- controllers/providerController.js
+++ controllers/providerController.js
@@ -626,7 +626,7 @@ exports.deleteFile = async (req, res) => {
 
 exports.getProvidersByBusinessType = async (req, res) => {
   try {
-    const { lat, lng, radius, businessType } = req.query;
+    const { lat, lng, radius, businessType } = req.body;
     if (!lat || !lng || !radius || !businessType) {
       return res.status(400).json({
         status: 400,
