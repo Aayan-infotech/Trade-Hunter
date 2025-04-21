2cece2e8c18a64e74a50e64624a54ea83759392c
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-04-21 14:02:15 +0530

    add xchnage

--- config/db.js
+++ config/db.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        // await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect('mongodb+srv://anuragyadav:hs0PwDnmUexc7zn7@cluster0.3h4gs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
         console.log('MongoDB connected...');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
