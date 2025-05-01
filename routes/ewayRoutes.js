6fcd27102c1eb5e4c685f122f7bc91728f9f3bb3
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-05-01 12:12:02 +0530

    Added changes

--- routes/ewayRoutes.js
+++ routes/ewayRoutes.js
@@ -3,12 +3,10 @@ const axios = require('axios');
 const router = express.Router();
 const ewayController = require('../controllers/ewayController');
 
-// Replace with actual API keys
 const API_KEY = 'C3AB9C5bfS+DBG9Rx9lZ2gZlXbX+ut8Xvni9NPznSIzTs6S7GUx4b+g2YPBTDnFjxRHzM8';
 const API_PASSWORD = 'a94hsJf1';
-const API_URL = 'https://api.sandbox.ewaypayments.com/Transaction'; // Make sure this is the correct endpoint
+const API_URL = 'https://api.sandbox.ewaypayments.com/Transaction'; 
 
-// Define the getAuthHeader function
 const getAuthHeader = () => {
   const authString = `${API_KEY}:${API_PASSWORD}`;
   const base64Auth = Buffer.from(authString).toString('base64');
