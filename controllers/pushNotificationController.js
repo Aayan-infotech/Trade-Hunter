b49c164284c163a145a3cec3cf8837d7529ec589
Rishabh Sharma <rishabh.sharma@aayaninfotech.com>
2025-05-30 20:20:22 +0530

    Added changes

--- controllers/pushNotificationController.js
+++ controllers/pushNotificationController.js
@@ -141,6 +141,8 @@ exports.sendPushNotificationAdmin = async (req, res) => {
     }
 
     // Emit to specific user room using their userId
+    const io = req.app.get("io");
+    io.emit( "Admin chat notification",notificationData);
 
     return res.status(200).json({
       status: 200,
